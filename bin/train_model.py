cd435c95ad910c7b8d55243a3df93b01d16ef54e
Hi-king <hikingko1@gmail.com>
2016-07-19 14:44:57 +0900

    :lipstick: check compile

--- bin/train_model.py
+++ bin/train_model.py
@@ -7,6 +7,10 @@ import random
 import numpy
 import chainer
 import chainer.optimizers
+import pipe
+import itertools
+import subprocess
+import tempfile
 
 rootpath = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(rootpath)
@@ -20,13 +24,13 @@ parser.add_argument("--epoch", type=int, default=10)
 parser.add_argument("--testnum", type=int, default=10)
 args = parser.parse_args()
 
+
 def data_list(dirpath, testnum=10):
     all_pathes = [os.path.join(dirpath, base) for base in os.listdir(dirpath)]
     random.shuffle(all_pathes)
     return all_pathes[:testnum], all_pathes[testnum:]
 positive_test, positive_train = data_list(os.path.join(rootpath, "data", args.contest, args.stage, args.language, "AC"), testnum=args.testnum)
 negative_test, negative_train = data_list(os.path.join(rootpath, "data", args.contest, args.stage, args.language, "WA"), testnum=args.testnum)
-
 
 class WholeProgramPredictor(object):
     def __init__(self, model, chara_encoder, xp=numpy):
@@ -54,7 +58,39 @@ class CharaEncoder(object):
 
 def data_loader(file_pathes):
     for file_path in file_pathes:
-        yield open(file_path).read()
+        print(file_path)
+        if (not args.language == "gpp_5.3.0") or check_compilable(replaced):
+            yield open(file_path).read().lower()
+
+def check_compilable(code):
+    f = tempfile.NamedTemporaryFile("w+", delete=False, suffix=".cpp")
+    f.write(code)
+    f.flush()
+    f.close()
+    process = subprocess.Popen([
+        "g++",
+        "-std=c++11",
+        f.name],
+        stderr=open(os.devnull, 'wb'))
+    return_code = process.wait()
+    return return_code == 0
+
+
+@pipe.Pipe
+def augmentation(iterable, num_augment=10):
+    charas = [chr(c) for c in range(ord("a"), ord("z") + 1)]
+    for code in iterable:
+        replacements = list(itertools.product(charas, repeat=2))
+        random.shuffle(replacements)
+        augmented = 0
+        for chara_from, chara_to in replacements:
+            replaced = code.replace(chara_from, chara_to)
+            if (not args.language == "gpp_5.3.0") or check_compilable(replaced):
+                yield replaced
+                augmented += 1
+            if augmented >= num_augment:
+                break
+
 
 def train(postivies, negatives, predictor, optimizer, batch=10, epoch_size=10000000):
     xp = predictor.xp
@@ -72,17 +108,17 @@ def train(postivies, negatives, predictor, optimizer, batch=10, epoch_size=10000
         loss.backward()
         optimizer.update()
 
-def test(postivies, negatives, predictor):
+def test(postivies, negatives, predictor, head=1000):
     data_loader_positive = data_loader(postivies)
     data_loader_negative = data_loader(negatives)
     num_correct = 0
     num_total = 0
-    for sample in data_loader_positive:
+    for sample in data_loader_positive | pipe.take(head):
         predicted = chainer.functions.softmax(predictor(sample))
         if numpy.argmax(predicted.data[0]) == 1:
             num_correct += 1
         num_total += 1
-    for sample in data_loader_negative:
+    for sample in data_loader_negative | pipe.take(head):
         predicted = chainer.functions.softmax(predictor(sample))
         if numpy.argmax(predicted.data[0]) == 0:
             num_correct += 1
@@ -95,7 +131,7 @@ predictor = WholeProgramPredictor(model, chara_encoder=CharaEncoder(), xp=numpy)
 optimizer = chainer.optimizers.Adam()
 optimizer.setup(model)
 for epoch in range(args.epoch):
-    train(positive_train, negative_train, predictor, optimizer, batch=1)
-    test(positive_train, negative_train, predictor)
+    train(positive_train, negative_train, predictor, optimizer, batch=1, epoch_size=10)
+    test(positive_train, negative_train, predictor, head=10)
     test(positive_test, negative_test, predictor)
 
